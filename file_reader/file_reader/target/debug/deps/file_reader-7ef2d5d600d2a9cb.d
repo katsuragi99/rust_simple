46dd85a2cf4bc53d051c99c9c7cd19aaaf8e0b2e
Katsuragi <katsuragikeima99@yandex.com>
2021-01-02 12:31:21 +0530

    testing

---
+++ file_reader/file_reader/target/debug/deps/file_reader-7ef2d5d600d2a9cb.d
@@ -0,0 +1,5 @@
+/home/kaguya/Documents/github/rust_simple/file_reader/file_reader/target/debug/deps/file_reader-7ef2d5d600d2a9cb: src/main.rs
+
+/home/kaguya/Documents/github/rust_simple/file_reader/file_reader/target/debug/deps/file_reader-7ef2d5d600d2a9cb.d: src/main.rs
+
+src/main.rs:
